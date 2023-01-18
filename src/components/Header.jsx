255acf8ba33aff58003a7fe4101617096b178095
Hritvik Mohan <hritvik5005@gmail.com>
2023-01-18 21:01:52 +0530

    Update: links open in a new tab

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,20 +17,20 @@ export default function Header() {
             <img className='profile-mod' src="https://pbs.twimg.com/profile_images/1564180600382054400/pQOmGGFo_400x400.jpg" alt="" srcset="" />
           </div>
           <div>
-            <a href="https://github.com/Hritvik-Mohan"><i class="bi bi-github"></i></a>
+            <a href="https://github.com/Hritvik-Mohan" target="_blank"><i class="bi bi-github"></i></a>
           </div>
           <div>
-            <a href="https://twitter.com/hritvik_io"><i class="bi bi-twitter"></i></a>
+            <a href="https://twitter.com/hritvik_io" target="_blank"><i class="bi bi-twitter"></i></a>
           </div>
           <div>
-            <a href="https://www.behance.net/hritvikmohan"><i class="bi bi-behance"></i></a>
+            <a href="https://www.behance.net/hritvikmohan" target="_blank"><i class="bi bi-behance"></i></a>
           </div>
           <div>
-            <a href="https://www.linkedin.com/in/hritvik-mohan-33162b131/"><i class="bi bi-linkedin"></i></a>
+            <a href="https://www.linkedin.com/in/hritvik-mohan-33162b131/" target="_blank"><i class="bi bi-linkedin"></i></a>
           </div>
         </div>
         <div className='resume'>
-          <a href='https://drive.google.com/file/d/1RGHNC1Gc7AuMY_fWJLtxrLNA9aN1xxhE/view?usp=sharing'>Resume</a>
+          <a href='https://drive.google.com/file/d/1H-3U0OWmvIwAfJRYK10pkj5Un-tFX8wA/view?usp=sharing' target="_blank">Resume</a>
         </div>
       </div>
     </div>
