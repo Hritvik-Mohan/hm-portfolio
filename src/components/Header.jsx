e232d4e7e3435b545951f8a40534adee31f9f4e1
Hritvik Mohan <hritvik5005@gmail.com>
2023-01-18 14:34:20 +0530

    Fix: UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
       <div className="name">
         <img src="https://pbs.twimg.com/profile_images/1564180600382054400/pQOmGGFo_400x400.jpg" alt="" srcset="" />
         <div className=''>
-          <h3>Hi, I'm Hritvik 🥤</h3>
+          <h3>Hi, I'm Hritvik🥤</h3>
           <p>Frontend Developer</p>
         </div>
       </div>
