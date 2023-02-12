ce7445803409e04936c24f4b0689959e01c5169b
Hritvik Mohan <hritvik5005@gmail.com>
2023-02-13 00:44:26 +0530

    fix: Projects css

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,6 +4,7 @@ function About() {
   return (
     <p className='about main-container'>
       <h2>Hi, I'm Hritvik Mohan</h2>
+      {/* <img src="https://pbs.twimg.com/profile_images/1564180600382054400/pQOmGGFo_400x400.jpg" alt="" /> */}
         <p>I'm a recent Bachelor of Computer Application grad, 2022. I have a strong passion for web development with a knack for design. </p>
         <p>I have experience with various front-end development technologies, including ReactJS, JavaScript, and HTML/CSS. </p>
     </p>
