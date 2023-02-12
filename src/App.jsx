06d369bc683dbd98fba944bb1d130edf277a7fa3
Hritvik Mohan <hritvik5005@gmail.com>
2023-02-13 00:49:55 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header'
-import data from "./data.json"
-import ProjectCard from './components/ProjectCard'
 import About from './components/About'
 import Education from './components/Education'
 import Home from './components/Home'
@@ -21,17 +19,18 @@ function App() {
         <h2 className='tabs'>
           {/* <span>All</span> */}
           <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
+          {/* <Link to="/about">About</Link> */}
           <Link to="/projects">Projects</Link>
-          <Link to="/blogs">Blogs</Link>
+          {/* <Link to="/blogs">Blogs</Link> */}
           {/* <span>UI UX</span> */}
           <Link to="/education">Education</Link>
           {/* <Link to="/experience">Experience</Link> */}
         </h2>
       </section>
       <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
+        {/* <Route exact path='/' element={<Home />} /> */}
+        <Route exact path='/' element={<About />} />
+        {/* <Route path='/about' element={<About />} /> */}
         <Route path='/education' element={<Education />} />
         <Route path='/projects' element={<Projects />} />
         <Route path='/blogs' element={<Blogs />} />
