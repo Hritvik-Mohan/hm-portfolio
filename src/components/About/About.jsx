2b1168d120f8807bb97eaf2860caa97766c2a71b
Hritvik Mohan <hritvik5005@gmail.com>
2023-02-13 20:09:54 +0530

    feat add header hr

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,15 +4,19 @@ import Education from '../Education/Education'
 function About() {
   return (
     <div className='about'>
-      <div className='bio-container'>
-        <div className='bio-img-head-wrapper'>
-          <img className='bio-img' src="https://pbs.twimg.com/profile_images/1564180600382054400/pQOmGGFo_400x400.jpg" alt="" />
-          <h2 className='bio-heading'>Hi, I'm Hritvik Mohan.</h2>
+      <div className='bio-img-text-container'>
+        <img className='bio-img' src="https://pbs.twimg.com/profile_images/1564180600382054400/pQOmGGFo_400x400.jpg" alt="" />
+        <div className='bio-container'>
+            <h2 className='bio-heading'>Hi, I'm Hritvik Mohan.</h2>
+            {/* <div className='bio-hr'></div> */}
+            <p>Welcome to my online home. I'm a Frontend Developer and a recent Bachelor of Computer Application grad, 2022. I have a strong passion for web development with a knack for design. </p>
+            <p>I have experience with various front-end development technologies, including ReactJS, JavaScript, and HTML/CSS. </p>
         </div>
-          <p>Welcome to my online home. I'm a Frontend Developer and a recent Bachelor of Computer Application grad, 2022. I have a strong passion for web development with a knack for design. </p>
-          <p>I have experience with various front-end development technologies, including ReactJS, JavaScript, and HTML/CSS. </p>
       </div>
-      {/* <Education /> */}
+      {/* <div className=''>
+        <h2 className='tabs-heading'>Education</h2>
+        <Education />
+      </div> */}
     </div>
   )
 }
