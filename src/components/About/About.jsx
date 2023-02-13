039b23c47a0a8cb6bc092314a9046645cd78d471
Hritvik Mohan <hritvik5005@gmail.com>
2023-02-13 18:44:41 +0530

    Update bio text and image

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,9 +5,11 @@ function About() {
   return (
     <div className='about'>
       <div className='bio-container'>
-        <h2 className='bio-heading'>Hi, I'm Hritvik Mohan</h2>
-        {/* <img src="https://pbs.twimg.com/profile_images/1564180600382054400/pQOmGGFo_400x400.jpg" alt="" /> */}
-          <p>I'm a Frontend Developer and a recent Bachelor of Computer Application grad, 2022. I have a strong passion for web development with a knack for design. </p>
+        <div className='bio-img-head-wrapper'>
+          <img className='bio-img' src="https://pbs.twimg.com/profile_images/1564180600382054400/pQOmGGFo_400x400.jpg" alt="" />
+          <h2 className='bio-heading'>Hi, I'm Hritvik Mohan.</h2>
+        </div>
+          <p>Welcome to my online home. I'm a Frontend Developer and a recent Bachelor of Computer Application grad, 2022. I have a strong passion for web development with a knack for design. </p>
           <p>I have experience with various front-end development technologies, including ReactJS, JavaScript, and HTML/CSS. </p>
       </div>
       {/* <Education /> */}
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
         {/* <img src="https://pbs.twimg.com/profile_images/1564180600382054400/pQOmGGFo_400x400.jpg" alt="" srcset="" /> */}
         <div className=''>
           {/* <h3>Hritvik Mohan</h3> */}
-          <h3>hm notes</h3>
+          <h3 className='logo'>hm notes</h3>
           {/* <h3>Hi, I'm Hritvik<span className='emoji'>🥤</span></h3> */}
           {/* <p>Frontend Developer</p> */}
         </div>
