f344e682a23a53692c9a0adb6a8a043ae50e6659
Hritvik Mohan <hritvik5005@gmail.com>
2023-02-22 18:24:46 +0530

    fix

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -5,7 +5,7 @@ function Education() {
     <div className='edu'>
         <h2 className='tabs-heading'>Education</h2>
         <div className=''>
-            <h2 className='edu-title'>Bachelore of Computer Application</h2>
+            <h2 className='edu-title'>Bachelor of Computer Application</h2>
             <p className='edu-institute'>School Of Management Sciences, Varanasi</p>
             <p className='edu-uni'>Mahatma Gandhi Kashi Vidyapith University</p>
             <p className='edu-duration'>2019 - 2022</p>
