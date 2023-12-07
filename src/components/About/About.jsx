3deea4811deb26793e2461160c48d97d2cd81148
Hritvik-Mohan <hritvik5005@gmail.com>
2023-12-07 15:07:38 +0530

    Add Experience Component/Section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,7 @@
 import './About.css'
 import Education from '../Education/Education'
 import Skills from '../Skills/Skills'
+import Experience from '../Experience/Experience'
 
 function About() {
   return (
@@ -10,13 +11,14 @@ function About() {
         <div className='bio-container'>
             <h2 className='bio-heading'>Hi, I'm Hritvik Mohan.</h2>
             {/* <div className='bio-hr'></div> */}
-            <p>Welcome to my online home. I'm a <span className='active-tab'>frontend developer</span> and a recent Bachelor of Computer Application grad, 2022, based in Varanasi. I have a strong passion for web development with a knack for <a href='https://www.behance.net/gallery/148350933/Auction-App' target="_blank" className='link'>design</a>. </p>
+            <p>Welcome to my online home. I'm a <span className='active-tab'>frontend developer</span> and a recent Bachelor of Computer Application grad, 2022, based in Bangalore. I have a strong passion for web development with a knack for <a href='https://www.behance.net/gallery/148350933/Auction-App' target="_blank" className='link'>design</a>. </p>
             <p>I have experience with various frontend development technologies, including ReactJS, JavaScript, HTML/CSS, and more.</p>
         </div>
       </div>
       <div className=''>
         <Skills />
         <Education />
+        <Experience/>
       </div>
     </div>
   )
---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,17 @@
+import './Experience.css'
+
+function Experience() {
+  return (
+    <div className='edu'>
+        <h2 className='tabs-heading'>Experience</h2>
+        <div className=''>
+            <h2 className='edu-title'>Newton School</h2>
+            <p className='edu-institute'>Technical Program Manager</p>
+            <p className='edu-uni'>Academics- Full Stack Development</p>
+            <p className='edu-duration'>2022</p>
+        </div>
+    </div>
+  )
+}
+
+export default Experience
