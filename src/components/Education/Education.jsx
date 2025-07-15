c704e34e384d5ee61d1c2c23ab6c0407dbf534cf
Hritvik-Mohan <hritvik5005@gmail.com>
2025-07-15 15:43:46 +0530

    Update Update Section

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -1,17 +1,23 @@
-import './Education.css'
+import "./Education.css";
 
 function Education() {
   return (
-    <div className='edu'>
-        <h2 className='tabs-heading'>Education</h2>
-        <div className=''>
-            <h2 className='edu-title'>Bachelor of Computer Application</h2>
-            <p className='edu-institute'>School Of Management Sciences, Varanasi</p>
-            <p className='edu-uni'>Mahatma Gandhi Kashi Vidyapith University</p>
-            <p className='edu-duration'>2019 - 2022</p>
-        </div>
+    <div className="edu">
+      <h2 className="tabs-heading">Education</h2>
+      <div className="">
+        <h2 className="edu-title">Master of Computer Application</h2>
+        <p className="edu-institute">Vellore Institute of Technology</p>
+        {/* <p className="edu-uni">Mahatma Gandhi Kashi Vidyapith University</p> */}
+        <p className="edu-duration">2025 - 2026</p>
+      </div>
+      <div className="">
+        <h2 className="edu-title">Bachelor of Computer Application</h2>
+        <p className="edu-institute">School Of Management Sciences, Varanasi</p>
+        <p className="edu-uni">Mahatma Gandhi Kashi Vidyapith University</p>
+        <p className="edu-duration">2019 - 2022</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Education
+export default Education;
